47c28ff9b94ebd596e57535248fe2d7912e2c653
haessae0 <haessae0@gmail.com>
2022-07-16 13:32:33 +0900

    start a project

---
+++ src/main/java/com/haessae0/usefuldiscordbot/UsefulDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package com.haessae0.usefuldiscordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulDiscordBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsefulDiscordBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haessae0/usefuldiscordbot/UsefulDiscordBotApplicationTests.java
@@ -0,0 +1,17 @@
+package com.haessae0.usefuldiscordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulDiscordBotApplicationTests {
+//GIVEN
+
+//WHEN
+
+    //TEHN
+    @Test
+    void contextLoads() {
+    }
+
+}
