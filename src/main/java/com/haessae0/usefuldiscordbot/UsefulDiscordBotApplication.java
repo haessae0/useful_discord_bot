96e503038c6283f200adb6af69be7983e4cd92ac
haessae0 <haessae0@gmail.com>
2022-07-16 14:20:47 +0900

    #1 - Completed connecting with discord
    must change MYTOKEN to discord token

--- src/main/java/com/haessae0/usefuldiscordbot/UsefulDiscordBotApplication.java
+++ src/main/java/com/haessae0/usefuldiscordbot/UsefulDiscordBotApplication.java
@@ -1,13 +1,26 @@
 package com.haessae0.usefuldiscordbot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 
-@SpringBootApplication
+import javax.security.auth.login.LoginException;
+
 public class UsefulDiscordBotApplication {
+    public UsefulDiscordBotApplication() throws LoginException, IllegalArgumentException {
+        JDABuilder builder = JDABuilder.createDefault(MYTOKEN);
 
-    public static void main(String[] args) {
-        SpringApplication.run(UsefulDiscordBotApplication.class, args);
+        builder.setActivity(Activity.listening("도움말 명령어는 hi!help"));
+        builder.setStatus(OnlineStatus.ONLINE);
+
+        builder.build();
     }
 
+    public static void main(String[] args) {
+        try {
+            new UsefulDiscordBotApplication();
+        } catch (LoginException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
 }
