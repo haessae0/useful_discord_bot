6b6aec471bb4373d71eb6b2854177eb871e6f6b3
haessae0 <haessae0@gmail.com>
2022-07-17 12:40:57 +0900

    #3 - 메인 어플리케이션에 추가

--- src/main/java/com/haessae0/usefuldiscordbot/UsefulDiscordBotApplication.java
+++ src/main/java/com/haessae0/usefuldiscordbot/UsefulDiscordBotApplication.java
@@ -1,5 +1,6 @@
 package com.haessae0.usefuldiscordbot;
 
+import com.haessae0.usefuldiscordbot.listener.CommandListener;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -8,10 +9,12 @@ import javax.security.auth.login.LoginException;
 
 public class UsefulDiscordBotApplication {
     public UsefulDiscordBotApplication() throws LoginException, IllegalArgumentException {
-        JDABuilder builder = JDABuilder.createDefault(MYTOKEN);
+        JDABuilder builder = JDABuilder.createDefault(Token.token);
 
         builder.setActivity(Activity.listening("도움말 명령어는 hi!help"));
         builder.setStatus(OnlineStatus.ONLINE);
+
+        builder.addEventListeners(new CommandListener());
 
         builder.build();
     }
